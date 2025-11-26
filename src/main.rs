ffb746ec46898c003286dee91819b9f15ba180f9
Evan Relf <evan@evanrelf.com>
2025-11-25 16:15:23 -0800

    Add `ratatui`

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,59 @@
+mod terminal;
+
 use clap::Parser as _;
+use ratatui::{
+    crossterm::{
+        self,
+        event::{Event, KeyCode, KeyModifiers},
+    },
+    prelude::*,
+};
+use std::process::ExitCode;
 
 #[derive(clap::Parser)]
 struct Args {}
 
-fn main() -> anyhow::Result<()> {
+fn main() -> anyhow::Result<ExitCode> {
     let _args = Args::parse();
-    println!("Hello, world!");
-    Ok(())
+
+    let mut terminal = terminal::init();
+
+    let mut state = State::default();
+
+    loop {
+        terminal.draw(|frame| {
+            let area = frame.area();
+            let buffer = frame.buffer_mut();
+            render(&state, area, buffer);
+        })?;
+
+        let event = crossterm::event::read()?;
+        if let Some(exit_code) = handle_event(&mut state, &event) {
+            return Ok(exit_code);
+        }
+    }
+}
+
+#[derive(Default)]
+struct State {}
+
+fn handle_event(_state: &mut State, event: &Event) -> Option<ExitCode> {
+    let mut exit_code = None;
+
+    #[expect(clippy::single_match)]
+    #[expect(clippy::match_same_arms)]
+    match event {
+        Event::Key(key_event) => match (key_event.modifiers, key_event.code) {
+            (KeyModifiers::CONTROL, KeyCode::Char('c')) => exit_code = Some(ExitCode::SUCCESS),
+            (KeyModifiers::NONE, KeyCode::Char('q')) => exit_code = Some(ExitCode::SUCCESS),
+            _ => {}
+        },
+        _ => {}
+    }
+
+    exit_code
+}
+
+fn render(_state: &State, area: Rect, buffer: &mut Buffer) {
+    Line::raw("Hello, world!").render(area, buffer);
 }
---
+++ src/terminal.rs
@@ -0,0 +1,40 @@
+use ratatui::crossterm::{
+    event::{DisableMouseCapture, EnableMouseCapture},
+    execute,
+};
+use std::{
+    io,
+    ops::{Deref, DerefMut},
+    thread,
+};
+
+pub struct TerminalGuard(pub ratatui::DefaultTerminal);
+
+#[must_use]
+pub fn init() -> TerminalGuard {
+    let terminal = ratatui::init();
+    execute!(io::stdout(), EnableMouseCapture).unwrap();
+    TerminalGuard(terminal)
+}
+
+impl Deref for TerminalGuard {
+    type Target = ratatui::DefaultTerminal;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for TerminalGuard {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl Drop for TerminalGuard {
+    fn drop(&mut self) {
+        if !thread::panicking() {
+            ratatui::restore();
+        }
+        let _ = execute!(io::stdout(), DisableMouseCapture);
+    }
+}
